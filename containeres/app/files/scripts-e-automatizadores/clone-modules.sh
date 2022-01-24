fe0821f8a32783387050aaeb6e5a9fa28890aff9
Carlos Eduardo <cadumsx@gmail.com>
2022-01-24 13:11:55 +0000

    feat: customizações para o TRE-CE

--- containeres/app/files/scripts-e-automatizadores/clone-modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /sei-modulos
-
-cd /sei-modulos
-
-git clone https://github.com/spbgovbr/mod-sei-estatisticas.git
-git clone https://github.com/spbgovbr/mod-wssei.git
-git clone https://github.com/spbgovbr/mod-sei-resposta.git
--- containeres/app/files/scripts-e-automatizadores/entrypoint.sh
+++ containeres/app/files/scripts-e-automatizadores/entrypoint.sh
@@ -134,52 +134,17 @@ echo "***************************************************"
 
 # Atualizacao do endereco de host da aplicacao
 echo "Atualizando Banco de Dados com as Configuracoes Iniciais..."
-if [ "$APP_DB_TIPO" == "MySql" ]; then
-    echo "Atualizando MySql..."
-    MYSQL_CMD="mysql --host $APP_DB_HOST --user $APP_DB_ROOT_USERNAME --password=$APP_DB_ROOT_PASSWORD"
-    $MYSQL_CMD -e "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" sip
-    $MYSQL_CMD -e "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" sei
-    $MYSQL_CMD -e "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP';" sip
-    $MYSQL_CMD -e "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI';" sip
-fi
 
 if [ "$APP_DB_TIPO" == "Oracle" ]; then
     echo "Atualizando Oracle..."
-    echo "alter user sip identified by sip_user;" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "alter user sei identified by sei_user;" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" | sqlplus64 sei/sei_user@$APP_DB_HOST
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP';" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI';" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
+#    echo "alter user sip identified by sip_user;" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
+#    echo "alter user sei identified by sei_user;" | sqlplus64 $APP_DB_ROOT_USERNAME/$APP_DB_ROOT_PASSWORD@$APP_DB_HOST
+    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" | sqlplus64 $APP_DB_SIP_USERNAME/$APP_DB_SIP_PASSWORD@$APP_DB_HOST
+#    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO';" | sqlplus64 sei/sei_user@$APP_DB_HOST
+    echo "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP';" | sqlplus64 $APP_DB_SIP_USERNAME/$APP_DB_SIP_PASSWORD@$APP_DB_HOST
+    echo "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI';" | sqlplus64 $APP_DB_SIP_USERNAME/$APP_DB_SIP_PASSWORD@$APP_DB_HOST
 fi
 
-if [ "$APP_DB_TIPO" == "SqlServer" ]; then
-    echo "Atualizando SqlServer..."
-    echo "use sip" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_ROOT_USERNAME -P $APP_DB_ROOT_PASSWORD
-
-
-    echo "use sei" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update orgao set sigla='$APP_ORGAO', descricao='$APP_ORGAO_DESCRICAO'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_SEI_USERNAME -P $APP_DB_SEI_PASSWORD
-
-    echo "use sip" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sip' where sigla='SIP'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_ROOT_USERNAME -P $APP_DB_ROOT_PASSWORD
-
-    echo "use sip" > /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    echo "update sistema set pagina_inicial='$APP_HOST_URL/sei/inicializar.php', web_service='$APP_HOST_URL/sei/controlador_ws.php?servico=sip' where sigla='SEI'" >> /tmp/update.tmp
-    echo "go" >> /tmp/update.tmp
-    cat /tmp/update.tmp | tsql -S $APP_DB_HOST -U $APP_DB_ROOT_USERNAME -P $APP_DB_ROOT_PASSWORD
-fi
 
 echo "***************************************************"
 echo "***************************************************"
@@ -239,180 +204,6 @@ echo "Atualizador finalizado procedendo com a subida do apache..."
 sleep 3
 
 echo "Apache no ar"
-
-echo "***************************************************"
-echo "***************************************************"
-echo "*INICIANDO CONFIGURACOES DO MODULO DE ESTATISTICAS*"
-echo "***************************************************"
-echo "***************************************************"
-
-if [ "$MODULO_ESTATISTICAS_INSTALAR" == "true" ]; then
-    
-    if [ ! -f /sei/controlador-instalacoes/instalado-modulo-estatisticas.ok ]; then
-    
-        if [ -z "$MODULO_ESTATISTICAS_VERSAO" ] || \
-           [ -z "$MODULO_ESTATISTICAS_URL" ] || \
-	       [ -z "$MODULO_ESTATISTICAS_SIGLA" ] || \
-	       [ -z "$MODULO_ESTATISTICAS_CHAVE" ]; then
-            echo "Informe as seguinte variaveis de ambiente no container:"
-            echo "MODULO_ESTATISTICAS_VERSAO, MODULO_ESTATISTICAS_URL, MODULO_ESTATISTICAS_SIGLA, MODULO_ESTATISTICAS_CHAVE"
-
-        else
-            
-            echo "Verificando existencia do modulo de estatisticas"
-            if [ -d "/opt/sei/web/modulos/mod-sei-estatisticas" ]; then
-                echo "Ja existe um diretorio para o modulo de estatisticas. Vamos assumir que o codigo la esteja integro"
-        
-            else
-                echo "Copiando o modulo de estatisticas"
-                cp -Rf /sei-modulos/mod-sei-estatisticas /opt/sei/web/modulos/
-            fi
-        
-
-            cd /opt/sei/web/modulos/mod-sei-estatisticas
-            git checkout $MODULO_ESTATISTICAS_VERSAO
-            echo "Versao do Governanca eh agora: $MODULO_ESTATISTICAS_VERSAO"
-
-            cd /opt/sei/
-        
-            sed -i "s#/\*novomodulo\*/#'MdEstatisticas' => 'mod-sei-estatisticas', /\*novomodulo\*/#g" config/ConfiguracaoSEI.php
-            sed -i "s#/\*extramodulesconfig\*/#'MdEstatisticas' => array('url' => '$MODULO_ESTATISTICAS_URL','sigla' => '$MODULO_ESTATISTICAS_SIGLA','chave' => '$MODULO_ESTATISTICAS_CHAVE'), /\*extramodulesconfig\*/#g" config/ConfiguracaoSEI.php
-        
-            
-        fi
-        
-    else
-    
-        echo "Arquivo de controle do Modulo de Estatisticas encontrado, provavelmente ja foi instalado, pulando configuracao do modulo"
-    
-    fi
-
-else
-    
-    echo "Variavel MODULO_ESTATISTICAS_INSTALAR nao setada para true, pulando configuracao..."
-    
-fi
-
-
-echo "***************************************************"
-echo "***************************************************"
-echo "**CONFIGURANDO MODULO WSSEI************************"
-echo "***************************************************"
-echo "***************************************************"
-if [ "$MODULO_WSSEI_INSTALAR" == "true" ]; then
-    
-    if [ ! -f /sei/controlador-instalacoes/instalado-modulo-wssei.ok ]; then
-        
-        if [ -z "$MODULO_WSSEI_VERSAO" ] || \
-           [ -z "$MODULO_WSSEI_URL_NOTIFICACAO" ] || \
-	       [ -z "$MODULO_WSSEI_ID_APP" ] || \
-	       [ -z "$MODULO_WSSEI_CHAVE" ]; then
-            echo "Informe as seguinte variaveis de ambiente no container:"
-            echo "MODULO_WSSEI_VERSAO, MODULO_WSSEI_URL_NOTIFICACAO, MODULO_WSSEI_ID_APP, MODULO_WSSEI_CHAVE"
-
-        else
-            
-            echo "Verificando existencia do modulo wssei"
-            if [ -d "/opt/sei/web/modulos/mod-wssei" ]; then
-                echo "Ja existe um diretorio para o modulo wssei. Vamos assumir que o codigo la esteja integro"
-        
-            else
-                echo "Copiando o modulo wssei"
-                cp -Rf /sei-modulos/mod-wssei /opt/sei/web/modulos/
-            fi
-        
-
-            cd /opt/sei/web/modulos/mod-wssei
-            git checkout $MODULO_WSSEI_VERSAO
-            echo "Versao do WSSEI eh agora: $MODULO_WSSEI_VERSAO"
-
-            cd /opt/sei/
-            sed -i "s#/\*novomodulo\*/#'MdWsSeiRest' => 'mod-wssei/', /\*novomodulo\*/#g" config/ConfiguracaoSEI.php
-            sed -i "s#/\*extramodulesconfig\*/#'WSSEI' => array('UrlServicoNotificacao' => '$MODULO_WSSEI_URL_NOTIFICACAO', 'IdApp' => '$MODULO_WSSEI_ID_APP', 'ChaveAutorizacao' => '$MODULO_WSSEI_CHAVE', 'TokenSecret' => '504CE1E9-8913-488F-AB3E-EDDABC065B0B'  ), /\*extramodulesconfig\*/#g" config/ConfiguracaoSEI.php
-
-            
-        fi
-        
-    else
-    
-        echo "Arquivo de controle do Modulo WSSEI encontrado pulando configuracao do modulo"
-    
-    fi
-
-else
-    
-    echo "Variavel MODULO_WSSEI_INSTALAR nao setada para true, pulando configuracao..."
-    
-fi
-
-echo "***************************************************"
-echo "***************************************************"
-echo "**CONFIGURANDO MODULO RESPOSTA*********************"
-echo "***************************************************"
-echo "***************************************************"
-if [ "$MODULO_RESPOSTA_INSTALAR" == "true" ]; then
-
-    if [ ! -f /sei/controlador-instalacoes/instalado-modulo-resposta.ok ]; then
-
-        if [ -z "$MODULO_RESPOSTA_VERSAO" ]; then
-            echo "Informe as seguinte variaveis de ambiente no container:"
-            echo "MODULO_RESPOSTA_VERSAO"
-
-        else
-
-            echo "Verificando existencia do modulo resposta"
-            if [ -d "/opt/sei/web/modulos/mod-sei-resposta" ]; then
-                echo "Ja existe um diretorio para o modulo resposta. Vamos assumir que o codigo la esteja integro"
-
-            else
-                echo "Copiando o modulo resposta"
-                cp -Rf /sei-modulos/mod-sei-resposta /opt/sei/web/modulos/
-            fi
-
-
-            cd /opt/sei/web/modulos/mod-sei-resposta
-            git checkout $MODULO_RESPOSTA_VERSAO
-            echo "Versao do WSSEI é agora: $MODULO_RESPOSTA_VERSAO"
-
-            cd /opt/sei/
-            sed -i "s#/\*novomodulo\*/#'MdRespostaIntegracao' => 'mod-sei-resposta/', /\*novomodulo\*/#g" config/ConfiguracaoSEI.php
-
-
-            TMPFILE_SEI=/opt/sei/web/modulos/mod-sei-resposta/sei_atualizar_versao_modulo_sei_resposta.php
-            if test -f "$TMPFILE_SEI"; then
-
-                # mover os scripts e executar
-                cp /opt/sei/web/modulos/mod-sei-resposta/sei_atualizar_versao_modulo_sei_resposta.php /opt/sei/scripts
-
-                echo "Vou rodar o script de atualizacao do modulo no SEI"
-                php -c /etc/php.ini /opt/sei/scripts/sei_atualizar_versao_modulo_sei_resposta.php
-            fi
-
-            TMPFILE_SIP=/opt/sei/web/modulos/mod-sei-resposta/sip_atualizar_versao_modulo_sei_resposta.php
-            if test -f "$TMPFILE_SIP"; then
-
-                # mover os scripts e executar
-                cp /opt/sei/web/modulos/mod-sei-resposta/sip_atualizar_versao_modulo_sei_resposta.php /opt/sip/scripts
-
-                echo "Vou rodar o script de atualizacao do modulo no SIP"
-                php -c /etc/php.ini /opt/sip/scripts/sip_atualizar_versao_modulo_sei_resposta.php
-            fi
-
-            touch /sei/controlador-instalacoes/instalado-modulo-resposta.ok
-
-        fi
-
-    else
-
-        echo "Arquivo de controle do Modulo RESPOSTA encontrado pulando configuracao do modulo"
-
-    fi
-
-else
-
-    echo "Variavel MODULO_RESPOSTA_INSTALAR nao setada para true, pulando configuracao..."
-
-fi
 
 
 touch /sei/controlador-instalacoes/instalado.ok
